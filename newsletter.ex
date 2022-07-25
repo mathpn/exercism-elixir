aaa8db120a395f1ab1a897035626ec46f27dbf32
Matheus Pedroni <pnmatheus@protonmail.com>
2022-07-24 22:16:23 -0300

    5 more elixir exercises

---
+++ newsletter.ex
@@ -0,0 +1,25 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    File.read!(path)
+    |> String.split()
+  end
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+  def log_sent_email(pid, email) do
+    IO.puts(pid, email)
+  end
+  def close_log(pid) do
+    File.close(pid)
+  end
+  def send_newsletter(emails_path, log_path, send_fun) do
+    emails = read_emails(emails_path)
+    log_file = open_log(log_path)
+    Enum.each(emails, fn email ->
+      if :ok == send_fun.(email) do
+        log_sent_email(log_file, email)
+      end
+    end)
+    close_log(log_file)
+  end
+end
